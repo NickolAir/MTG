5552f7ab9b82414a2b222c66618a544e072e0b04
nikolayratushnyak <nikolay.ratushnyak03@gmail.com>
2024-12-08 22:04:20 +0700

    added serch bar

--- MTG/CardCollection/CardCollectionViewModel.swift
+++ MTG/CardCollection/CardCollectionViewModel.swift
@@ -5,13 +5,13 @@ class CardCollectionViewModel {
     var isOnlineSource = false
     private var allCards: [CardModel] = [] // Полный список карт из модели
     var cards: [CardModel] = [] // Текущий список для отображения
-
+    
     var onCardsUpdated: (() -> Void)? // Callback для обновления UI
     private var cancellables: Set<AnyCancellable> = [] // Хранилище подписок Combine
-
-    func loadCards(){
+    
+    func loadCards() {
         print("Loading cards")
-
+        
         HTTPService.shared.fetchAllCards(from: URL(string: "http://37.46.135.99")!)
             .sink(receiveCompletion: { completion in
                 switch completion {
@@ -25,9 +25,21 @@ class CardCollectionViewModel {
                 cards.forEach { card in
                     print("ID: \(card.id), Name: \(card.name)")
                 }
-                self.cards = cards
+                self.allCards = cards // Сохраняем полный список карт
+                self.cards = cards // Изначально показываем все карты
                 self.onCardsUpdated?()
             })
             .store(in: &cancellables) // Сохраняем подписку
+    }
+    
+    func filterCards(by query: String) {
+        if query.isEmpty {
+            cards = allCards // Если строка поиска пустая, показываем все карты
+        } else {
+            cards = allCards.filter { card in
+                card.name!.lowercased().contains(query.lowercased()) // Фильтрация по имени
+            }
+        }
+        onCardsUpdated?() // Уведомляем UI об обновлении данных
     }
 }
--- MTG/CardCollection/CardCollectionWindowViewController.swift
+++ MTG/CardCollection/CardCollectionWindowViewController.swift
@@ -5,14 +5,16 @@ class CardCollectionWindowViewController: UIViewController {
     private let viewModel = CardCollectionViewModel()
     
     var onCardSelected: ((CardModel) -> ())?
-
     
-    private lazy var mainView: UIView = {
-       let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+    // Поисковая строка
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for cards"
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.delegate = self
+        return searchBar
     }()
-
+    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         
@@ -32,25 +34,31 @@ class CardCollectionWindowViewController: UIViewController {
         collectionView.dataSource = self
         return collectionView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         bindViewModel()
         viewModel.loadCards() // Запрос данных из ViewModel
     }
-
+    
     private func setupUI() {
+        view.addSubview(searchBar)
         view.addSubview(collectionView)
         view.backgroundColor = .white
+        
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leftAnchor.constraint(equalTo: view.leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: view.rightAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             collectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
             collectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
         ])
     }
-
+    
     private func bindViewModel() {
         viewModel.onCardsUpdated = { [weak self] in
             DispatchQueue.main.async {
@@ -79,5 +87,22 @@ extension CardCollectionWindowViewController: UICollectionViewDelegate, UICollec
         let selectedCard = viewModel.cards[indexPath.row]
         onCardSelected?(selectedCard) // Уведомляем другой экран о выборе карты
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension CardCollectionWindowViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.filterCards(by: searchText) // Передаём строку поиска в ViewModel
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder() // Скрываем клавиатуру при нажатии кнопки поиска
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        viewModel.filterCards(by: "") // Сбрасываем поиск
+        searchBar.resignFirstResponder() // Скрываем клавиатуру
     }
 }
